dfd14e3c67950b104d8470ade5cf1210ebb21d3d
Ayomisco <fayomide324@gmail.com>
2021-08-25 02:34:31 +0100

    Making some simple changes

--- ayomide_resume/settings.py
+++ ayomide_resume/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-/8
+
 STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
